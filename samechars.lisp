66eae2c359bf245eb72ddacb090ddfebe12ee9f7
otjura <ojrajala@gmail.com>
2016-02-11 16:37:27 +0200

    renamed to not conflict with core common-lisp function

--- samechars.lisp
+++ samechars.lisp
@@ -3,5 +3,8 @@
    for example if arg0 is "tree" and arg1 "free" program returns "ree", but
    if arg1 is "fred" it returns "re". |#
 
-(defun intersection (arg0 arg1)
-  (
+(defun samechars (arg0 arg1)
+  (declare (type (string) arg0 arg1))
+  (if (string= arg0 arg1)
+      arg0
+      nil))
