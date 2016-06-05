aae4c9a8248010cb4a0e8a283ef8a83fdaa2e94e
otjura <ojrajala@gmail.com>
2016-06-05 19:38:57 +0300

    deleted unnecessary dupefile

--- demo42.lisp
+++
@@ -1,6 +0,0 @@
-;; DEMO 4.2.
-;; Write method that returns new int array from parametre array,
-;; where primitives are sorted from largest to smallest,
-;; Write sorter self without using inbuilt methods.
-
-(defun array-sorter () 
